72de00374914b6f86744cfbb6ea95667fbfcd645
Marcel Canhisares <marcelcanhisares@MacBook-Air-de-Karla.local>
2016-01-08 17:15:45 -0200

    First commit (First game)

--- tapper-extreme/ViewController.swift
+++ tapper-extreme/ViewController.swift
@@ -10,16 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //Properties
+    var maxTaps = 0
+    var currentTaps = 0
+    
+    //Outlets
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var howManyTapsTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var tapBtn: UIButton!
+    @IBOutlet weak var tapsLbl: UILabel!
+    
+
+    @IBAction func onCoinTapped(sender: UIButton!) {
+        if (currentTaps <= maxTaps){
+            currentTaps++
+            tapsLbl.text = "\(currentTaps) Taps"
+        }else{
+            howManyTapsTxt.text = ""
+            
+            tapsLbl.hidden = true
+            tapBtn.hidden = true
+            
+            logoImg.hidden = false
+            howManyTapsTxt.hidden = false
+            playBtn.hidden = false
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onPlayBtnPressed(sender: UIButton!){
+        if howManyTapsTxt.text != nil && howManyTapsTxt.text != ""{
+            
+            logoImg.hidden = true
+            howManyTapsTxt.hidden = true
+            playBtn.hidden = true
+            
+            tapBtn.hidden = false
+            tapsLbl.hidden = false
+            
+            maxTaps = Int(howManyTapsTxt.text!)!
+            currentTaps = 0;
+            
+            tapsLbl.text = "\(currentTaps) Taps"
+        }
+        
+        
     }
-
-
 }
 
